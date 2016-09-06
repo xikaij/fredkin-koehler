f8a344aa2f489469204ab0779a011de84faf06a5
Xikai Jiang <jxk28@msn.com>
2016-09-06 10:32:17 -0500

    userobject

---
+++ include/userobjects/BoundaryIntegralFMM.h
@@ -0,0 +1,16 @@
+#ifndef BOUNDARYINTEGRALFMM_H
+#define BOUNDARYINTEGRALFMM_H
+
+#include "GeneralUserObject.h"
+
+class BoundaryIntaegralFMM;
+
+template<>
+InputParameters validParams<BoundaryIntegralFMM>();
+
+class BoundaryIntegralFMM : public GeneralUserObject
+{
+
+};
+
+#endif

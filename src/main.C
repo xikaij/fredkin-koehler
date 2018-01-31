7034b32139024e3ca160723977f7b94b0d857504
xikaij <xikai@anl.gov>
2018-01-30 21:39:17 -0600

    Correct file names.

--- src/main.C
+++ src/main.C
@@ -1,11 +1,11 @@
-#include "PoissonApp.h"
+#include "FredkinKoehlerApp.h"
 #include "MooseInit.h"
 #include "Moose.h"
 #include "MooseApp.h"
 #include "AppFactory.h"
 
 // Create a performance log
-PerfLog Moose::perf_log("Poisson");
+PerfLog Moose::perf_log("FredkinKoehler");
 
 // Begin the main program.
 int main(int argc, char *argv[])
@@ -14,10 +14,10 @@ int main(int argc, char *argv[])
   MooseInit init(argc, argv);
 
   // Register this application's MooseApp and any it depends on
-  PoissonApp::registerApps();
+  FredkinKoehlerApp::registerApps();
 
   // This creates dynamic memory that we're responsible for deleting
-  MooseApp * app = AppFactory::createApp("PoissonApp", argc, argv);
+  MooseApp * app = AppFactory::createApp("FredkinKoehlerApp", argc, argv);
 
   // Execute the application
   app->run();
